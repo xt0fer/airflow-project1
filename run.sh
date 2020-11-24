ff796f4e60923f4c1098b886d9698ea77b5a7027
Kristofer Younger <kryounger@gmail.com>
2020-11-24 13:39:13 -0500

    connect plugins folder

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker run --name airflow \
--v /home/kristofer/p/af-p1/airflow-project1/dags:/app/airflow/dags \
+       -v $(pwd)/dags:/app/airflow/dags \
+       -v $(pwd)/plugins/:/usr/local/airflow/plugins \
 -p 8080:8080 -d airflow:0.1.0
